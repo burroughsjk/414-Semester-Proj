779842636717909f21adb8c2b76281c4e0f74584
burroughsjk <burroughsjk@vcu.edu>
2023-04-21 18:20:33 -0400

    Added program runtime to summary

--- src/subdomain_enum.py
+++ src/subdomain_enum.py
@@ -1,5 +1,6 @@
 import dns.resolver
 import sys
+import time
 # Subdomain list sourced from https://github.com/rbsec/dnscan/blob/master/subdomains-10000.txt
 
 
@@ -11,20 +12,23 @@ def logException(subdom, exceptionType, file):
 		print('[NOT FOUND - ' + exceptionType + '] ' + f'{subdom}.{domain}')
 		file.write('[NOT FOUND - ' + exceptionType + '] ' + f'{subdom}.{domain} \n')
 
-def summarize(num_total, num_valid, num_invalid, file):
+def summarize(num_total, num_valid, num_invalid, runtime, file):
 	print('\n')
 	print('---------------- Summary ----------------')
+	print('Total runtime: ' + str(runtime) + ' sec')
 	print('Total subdomains checked: ' + str(num_total))
 	print('Total valid subdomains found: ' + str(num_valid))
 	print('Total invalid subdomains: ' + str(num_invalid))
 	
 	file.write('\n')
 	file.write('---------------- Summary ----------------\n')
+	file.write('Total runtime: ' + str(runtime) + ' sec\n')
 	file.write('Total subdomains checked: ' + str(num_total) + '\n')
 	file.write('Total valid subdomains found : ' + str(num_valid) + '\n')
 	file.write('Total invalid subdomains: ' + str(num_invalid) + '\n')
 	
 def main():
+	start_time = time.time()
 	count = 0
 	count_valid = 0
 	count_invalid = 0
@@ -42,6 +46,7 @@ def main():
 						count_valid += 1
 					else:
 						pass
+			# Exception Handling
 			except dns.resolver.NXDOMAIN:
 				logException(subdoms, "NXDOMAIN", l)
 				count_invalid += 1
@@ -55,13 +60,15 @@ def main():
 				count_invalid += 1
 				pass
 			except KeyboardInterrupt:
+				total_runtime = (time.time() - start_time)
 				print("---------------- Keyboard interrupt detected - program killed ----------------")
 				l.write("---------------- Keyboard interrupt detected - program killed ----------------\n")
-				summarize(count, count_valid, count_invalid, l)
+				summarize(count, count_valid, count_invalid, total_runtime, l)
 				quit()
 			count += 1
-
+	
+	total_runtime = (time.time() - start_time)
 	with open('log.txt', 'a') as l:
-		summarize(count, count_valid, count_invalid, l)
+		summarize(count, count_valid, count_invalid, total_runtime, l)
 
 main()
