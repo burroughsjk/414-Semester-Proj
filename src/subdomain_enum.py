c7a12b889ed2cbc1300b9a750e6ea2f34010322c
burroughsjk <burroughsjk@vcu.edu>
2023-04-21 17:15:32 -0400

    Now generates log file, also strmlined exceptions

--- src/subdomain_enum.py
+++ src/subdomain_enum.py
@@ -4,29 +4,40 @@ import sys
 
 
 domain = sys.argv[1]
-subdomain_array = ['lib', 'a', 'dns', 'open', 'm', 'img', 'studio', 'tv']
+subdomain_array = ['lib', 'img', 'dns', 'open', 'm', 'a', 'studio', 'tv']
 
 
 def main():
     subdomain_store = []
-    with open('output.txt', 'w') as f:  
+    with open('output.txt', 'w') as f, open('log.txt', 'w') as l:  
         for subdoms in subdomain_array:
             try:
                 ip_value = dns.resolver.resolve(f'{subdoms}.{domain}', 'A')
                 if ip_value:
                     subdomain_store.append(f'{subdoms}.{domain}')
                     if f'{subdoms}.{domain}' in subdomain_store:
-                        print(f'{subdoms}.{domain} valid')
+                        print(f'[VALID] {subdoms}.{domain}')
                         f.write(f'{subdoms}.{domain} \n')
+                        l.write(f'[VALID] {subdoms}.{domain} \n')
                     else:
                         pass
             except dns.resolver.NXDOMAIN:
-                print("Subdomain not found!")
+                logException(subdoms, "NXDOMAIN", l)
                 pass
             except dns.resolver.NoAnswer:
+                logException(subdoms, "NoAnswer", l)
+                pass
+            except dns.resolver.Timeout:
+                logException(subdoms, "Timeout", l)
                 pass
             except KeyboardInterrupt:
                 print("--------- Keyboard interrupt detected - program killed ---------")
+                l.write("--------- Keyboard interrupt detected - program killed ---------")
                 quit()
+
+
+def logException(subdom, exceptionType, file):
+        print('[NOT FOUND - ' + exceptionType + '] ' + f'{subdom}.{domain}')
+        file.write('[NOT FOUND - ' + exceptionType + '] ' + f'{subdom}.{domain} \n')
 
 main()
